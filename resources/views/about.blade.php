d210f8d095052675a160551a3f21e52991d53ea5
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-23 14:52:59 +0200

    Created about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daneric - About</title>
+    <link rel="shortcut icon" type="image/jpg" href="https://daneric.dev/images/logo.jpg">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <ul class="navtop">
+            <li><a class="navbar-brand" href="index.html"><img src="https://daneric.dev/images/logo.jpg" class="imgnav"><span class="navbar-brand-name">Daneric Aalbers</span></a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul class="topnav">
+            <li><a class="nav-btn" href="/">Home</a></li>
+            <li><a class="nav-btn active" href="/about">Over mij</a></li>
+            <li><a class="nav-btn" href="/projects">Projecten</a></li>
+            <li class="social-icons">
+                <a class="center" href="https://discord.gg/vX2DCW6"><i class="fab fa-discord"></i></a>
+            </li>
+            <li class="social-icons">
+                <a class="center" href="https://github.com/danericnetwork"><i class="fab fa-github"></i></a>
+            </li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
