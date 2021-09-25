7003cdcb8851bb994e9bd90e14200861c0125239
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-25 20:54:55 +0200

    Changed some code

---
+++ resources/views/parts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daneric - @yield('title')</title>
+    <link rel="shortcut icon" type="image/jpg" href="https://daneric.dev/images/Logo.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+    <!-- begin of metadata -->
+    <meta property="og:title" content="Daneric">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="/">
+    <meta property="og:image" content="https://daneric.dev/images/Logo.png">
+    <meta property="og:description" content="Daneric's Portfolio">
+    <meta name="theme-color" content="#0362fc">
+    <!-- end of metadata -->
+</head>
+<body>
+@yield('content')
+<div class="footer">
+    <div class="footer-sec section-1">
+        <p>Copyright © 2021 <a href="https://daneric.dev/">Daneric</a>, All rights reserved.</p>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -1,6 +1,25 @@
 @extends('parts.main')
-@section('title', 'Portfolio')
-@section('content')
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @section('title', 'Portfolio')
+    <link rel="shortcut icon" type="image/jpg" href="https://daneric.dev/images/Logo.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+    <!-- begin of metadata -->
+    <meta property="og:title" content="Daneric">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="/">
+    <meta property="og:image" content="https://daneric.dev/images/Logo.png">
+    <meta property="og:description" content="Daneric's Portfolio">
+    <meta name="theme-color" content="#0362fc">
+    <!-- end of metadata -->
+</head>
+<body>
     <nav>
         <ul class="navtop">
             <li><a class="navbar-brand"><img src="https://daneric.dev/images/Logo.png" class="imgnav"><span class="navbar-brand-name">Daneric Aalbers</span></a></li>
@@ -36,4 +55,5 @@
 <div class="card">
     <a href="https://doei.nl" class="btn btn-primary">doei</a>
 </div>
-@endsection
+</body>
+</html>
