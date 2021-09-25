812729a480ff952f428419a7198446432d58682d
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-25 22:27:55 +0200

    Added first page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('parts.main')
+@section('title', 'Home')
+@section('content')
+
+<a href="/nl/" class="btn btn-primary">Dutch</a>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/nl', function () {
     return view('dutch.index');
 });
