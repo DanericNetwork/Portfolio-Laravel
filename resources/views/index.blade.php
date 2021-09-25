046dcacb38315b62c4b9f07366863a23472078bf
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-25 22:46:15 +0200

    main page lil better

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,6 @@
 
 <a href="/nl/" class="btn btn-primary">Dutch</a>
 <br>
-<img style="width: 100%;" src="https://image.shutterstock.com/image-photo/mature-man-using-computer-drinking-260nw-119815924.jpg"></img>
+<img style="width: 80%; display: flex; margin: auto; height: 333px;" src="https://image.shutterstock.com/image-photo/mature-man-using-computer-drinking-260nw-119815924.jpg"></img>
 <h1>credits naar <a style="color: tomato;" href="https://twitter.com/mawmet">tim</a> voor het geweldige plaatje</h1>
 @endsection
