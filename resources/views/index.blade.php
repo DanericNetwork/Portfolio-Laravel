b6a0fe238c4046df4afdbc66e8244d27f7f94879
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-27 11:49:38 +0200

    Updated landingspage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,16 @@
 @extends('parts.main')
 @section('title', 'Home')
 @section('content')
-
-<a href="/nl/" class="btn btn-primary">Dutch</a>
+<div class="langg">
+    <div>
+        <h1 class="select-lang">Select your language</h1>
+    </div>
+        <div class="lang-btns">
+            <a href="/nl/" class="btn btn-language">Dutch</a>    
+            <a href="/en/" class="btn btn-language">English</a>
+        </div>  
+</div>
 <br>
-<img style="width: 80%; display: flex; margin: auto; height: 333px;" src="https://image.shutterstock.com/image-photo/mature-man-using-computer-drinking-260nw-119815924.jpg"></img>
-<h1>credits naar <a style="color: tomato;" href="https://twitter.com/mawmet">tim</a> voor het geweldige plaatje</h1>
+<!-- <img style="width: 80%; display: flex; margin: auto; height: 333px;" src="https://image.shutterstock.com/image-photo/mature-man-using-computer-drinking-260nw-119815924.jpg"></img>
+<h1>credits naar <a style="color: tomato;" href="https://twitter.com/mawmet">tim</a> voor het geweldige plaatje</h1> -->
 @endsection
