494ed88dfc70bec1597008b37332b4feeb91f5c5
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-30 13:44:38 +0200

    Fixed some responsive things & projects page

--- resources/views/dutch/projects.blade.php
+++ resources/views/dutch/projects.blade.php
@@ -8,7 +8,7 @@
 <div class="projects-container">
     <div class="project">
 
-        <div class="col-6 project-text">
+        <div class="project-text">
 
             <a class="project-title" style="color: #34c0eb !important;" href="https://disbots.net/bot/funkie">Funkie</a>
             <h3 class="project-desc">Uitgebracht op 18th Maart 2021.</h3>
@@ -18,10 +18,30 @@
 
         <div class="about-me image wow">
 
-            <img class="right-picture wow skeleton" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+
+        </div>
+    </div>
+
+    
+    <hr class="line">
+
+
+    <div class="project">
+
+        <div class="project-text">
+
+            <a class="project-title" style="color: #34c0eb !important;" href="https://disbots.net/bot/funkie">Funkie</a>
+            <h3 class="project-desc">Uitgebracht op 18th Maart 2021.</h3>
+            <p class="project-description">Funkie is een multipurpose Discord bot met meer dan 110 servers</p>
 
         </div>
 
+        <div class="about-me image wow">
+
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+
+        </div>
     </div>
  </div>
 
