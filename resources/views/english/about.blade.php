d16304b5853b5eb6fa866e24661f2d41eb596682
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-09-30 09:13:35 +0200

    about pagew

---
+++ resources/views/english/about.blade.php
@@ -0,0 +1,15 @@
+@extends('parts.main')
+@section('title', 'Home')
+@section('nav.about', 'active')
+
+@section('content')
+
+@include('parts.naven')
+<!-- Content section -->
+<h1 class="page-title">About me</h1>
+
+<div class="about">
+    <p class="text-about">Hey! My name is Daneric Aalbers. I am a 17 year old boy from the Netherlands who lives in Dedemsvaart. Currently I'm studying Software Developer at Landstede in Zwolle.</p>
+    <img class="image-about" src="../images/logo.png">
+</div>
+@endsection
