7e80916b17cf3af9a620c06f88033a99a47b29cb
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-10-04 10:09:49 +0200

    Made page title

--- resources/views/dutch/about.blade.php
+++ resources/views/dutch/about.blade.php
@@ -4,6 +4,8 @@
 @section('nav.about', 'active')
 
 @include('parts.navnl')
+<h1 class="page-title">Over mij</h1>
+
     <div class="about">
         <p class="text-about">Hey! Ik ben Daneric, 17 jaar en op dit moment studeer ik in zwolle op de opleiding Software Developer. In mijn vrije tijd werk ik graag aan discord bots en websites.</p>
         <img class="image-about" src="../images/logo.png">
