25ff9b52a8d3d1dbc8524abe2c71b53ecc0bb22f
Student Daneric Aalbers <daalbers0118@student.landstede.nl>
2021-10-05 09:20:32 +0200

    fix

--- app/Http/Controllers/BtcController.php
+++ app/Http/Controllers/BtcController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\HTTP;
+use Illuminate\Support\Facades\Http;
 
 class BtcController extends Controller
 {
