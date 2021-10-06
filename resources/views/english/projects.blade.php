67d82883f340474426b9e83580c089911b93c49a
DanericNetwork <danericaalbers@hotmail.com>
2021-10-06 11:22:05 +0200

    Made projects page in english

---
+++ resources/views/english/projects.blade.php
@@ -0,0 +1,48 @@
+@extends('parts.main')
+@section('title', 'Projects')
+@section('content')
+@section('nav.projects', 'active')
+
+@include('parts.naven')
+
+<div class="projects-container">
+    <div class="project">
+
+        <div class="project-text">
+
+            <a class="project-title" href="https://disbots.net/bot/funkie">Funkie</a>
+            <h3 class="project-desc">Launched 18th March 2021.</h3>
+            <p class="project-description">Funkie is a multipurpose Discord bot trusted by 110+ servers</p>
+
+        </div>
+
+        <div class="about-me image wow">
+
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+
+        </div>
+    </div>
+
+    
+    <hr class="line">
+
+
+    <div class="project">
+
+        <div class="project-text">
+
+            <a class="project-title" href="https://disbots.net/bot/funkie">Funkie</a>
+            <h3 class="project-desc">Launched 18th March 2021.</h3>
+            <p class="project-description">Funkie is a multipurpose Discord bot trusted by 110+ servers</p>
+
+        </div>
+
+        <div class="about-me image wow">
+
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+
+        </div>
+    </div>
+ </div>
+
+@endsection
