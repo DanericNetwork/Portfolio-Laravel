aa346273372fb6b0ae638c99216b9a585e22a489
DanericNetwork <danericaalbers@hotmail.com>
2021-10-25 11:27:47 +0200

    Removed "Hey!"

--- resources/views/dutch/about.blade.php
+++ resources/views/dutch/about.blade.php
@@ -7,7 +7,7 @@
 <h1 class="page-title">Over mij</h1>
 
     <div class="about">
-        <p class="text-about">Hey! Ik ben Daneric, 17 jaar en op dit moment studeer ik in zwolle op de opleiding Software Developer. In mijn vrije tijd werk ik graag aan discord bots en websites.</p>
+        <p class="text-about">Ik ben Daneric, 17 jaar en op dit moment studeer ik in Zwolle op de opleiding Software Developer. In mijn vrije tijd werk ik graag aan discord bots en websites.</p>
         <img class="image-about" src="../images/logo.png">
     </div>
 @endsection
--- resources/views/english/about.blade.php
+++ resources/views/english/about.blade.php
@@ -9,7 +9,7 @@
 <h1 class="page-title">About me</h1>
 
 <div class="about">
-    <p class="text-about">Hey! My name is Daneric Aalbers. I am a 17 year old boy from the Netherlands who lives in Dedemsvaart. Currently I'm studying Software Developer at Landstede in Zwolle.</p>
+    <p class="text-about">My name is Daneric Aalbers. I am a 17 year old boy from the Netherlands who lives in Dedemsvaart. Currently I'm studying Software Developer at Landstede in Zwolle.</p>
     <img class="image-about" src="../images/logo.png">
 </div>
 @endsection
