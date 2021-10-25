337b6c6faa2ede75ca248b299f1b25849bf3840f
DanericNetwork <danericaalbers@hotmail.com>
2021-10-25 11:27:27 +0200

    avg age

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -15,4 +15,29 @@ class DayController extends Controller
             return view('day')->with(['werkdag'=>$werkdag]);
         }
     }
+
+    public function avgAge()
+{
+//array: lijst van waarden 
+
+$leeftijden = ["", ""]; 
+
+$aantalStudenten = 34;
+//loop: while, for, foreach
+
+
+$totalAge = 0; 
+foreach ($leeftijden as $leeftijd) {
+$totalAge = $totalAge + $leeftijd;
+}
+
+//count telt het aantal woorden in een array (lijst)
+$aantalStudenten = $count($leeftijden);
+$avgAgw = $totalAge / $aantalStudenten; 
+
+// laat zien op het scherm 
+
+echo $avgAge;
+
+}
 }
