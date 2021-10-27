be9f866647dfb79a48dbbe2b7113a1dd5d6334b2
DanericNetwork <danericaalbers@hotmail.com>
2021-10-27 09:35:14 +0200

    Added scrollreveal to Projects page

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,13 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.info-text',{delay: 200}); 
 sr.reveal('.header-text',{delay: 400}); 
+sr.reveal('.image-about',{interval: 200}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.text-about',{delay: 200}); 
+
+// SCROLL PROJECT
+sr.reveal('.project-description',{delay: 200});
+sr.reveal('.project-desc',{delay: 300});
+sr.reveal('.project-title',{delay: 400});
+sr.reveal('.right-picture',{delay: 100, origin: 'right'});
