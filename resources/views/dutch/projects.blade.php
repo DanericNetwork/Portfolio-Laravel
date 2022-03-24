9983e608bde4be450db1f3b31709badbb83c9e9d
Daneric <danericaalbers@hotmail.com>
2022-03-24 13:34:43 +0100

    feat: added sharex uploader

--- resources/views/dutch/projects.blade.php
+++ resources/views/dutch/projects.blade.php
@@ -10,6 +10,44 @@
 
         <div class="project-text">
 
+            <a class="project-title" href="https://mojimind.nl">MojiMind</a>
+            <h3 class="project-desc">Uitgebracht 3 December 2021.</h3>
+            <p class="project-description">Mojimind is een herimplementatie van het klassieke spelletje mastermind, dit project heb ik samen met een klasgenoot gemaakt.</p>
+
+        </div>
+
+        <div class="about-me image wow">
+
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.my-project.nl/cdn/24aaf11fed61be9b" alt="Daneric">
+
+        </div>
+    </div>
+
+    <hr class="line">
+
+    <div class="project">
+
+        <div class="project-text">
+
+            <a class="project-title" href="https://cdn.my-project.nl">ShareX Uploader</a>
+            <h3 class="project-desc">Uitgebracht op 16 Maart 2022.</h3>
+            <p class="project-description">Dit is een image uploader voor het screenshot programma ShareX, ik heb dit project gemaakt in Javascript met ExpressJS.</p>
+
+        </div>
+
+        <div class="about-me image wow">
+
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.my-project.nl/cdn/1f3c719723e10893" alt="Daneric">
+
+        </div>
+    </div>
+
+    <hr class="line">
+
+    <div class="project">
+
+        <div class="project-text">
+
             <a class="project-title" href="https://disbots.net/bot/funkie">Funkie</a>
             <h3 class="project-desc">Uitgebracht op 18th Maart 2021.</h3>
             <p class="project-description">Funkie is een multipurpose Discord bot met meer dan 110 servers</p>
@@ -24,25 +62,8 @@
     </div>
 
 
-    <hr class="line">
 
 
-    <div class="project">
-
-        <div class="project-text">
-
-            <a class="project-title" href="https://mojimind.nl">MojiMind</a>
-            <h3 class="project-desc">Uitgebracht 3 December 2021.</h3>
-            <p class="project-description">Mojimind is een herimplementatie van het klassieke spelletje mastermind, dit project heb ik samen met een klasgenoot gemaakt.</p>
-
-        </div>
-
-        <div class="about-me image wow">
-
-            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.my-project.nl/cdn/24aaf11fed61be9b" alt="Daneric">
-
-        </div>
-    </div>
  </div>
 
 @endsection
--- resources/views/english/projects.blade.php
+++ resources/views/english/projects.blade.php
@@ -10,22 +10,20 @@
 
         <div class="project-text">
 
-            <a class="project-title" href="https://disbots.net/bot/funkie">Funkie</a>
-            <h3 class="project-desc">Launched 18th March 2021.</h3>
-            <p class="project-description">Funkie is a multipurpose Discord bot trusted by 110+ servers</p>
+            <a class="project-title" href="https://cdn.my-project.nl">ShareX Uploader</a>
+            <h3 class="project-desc">Uitgebracht op 16 Maart 2022.</h3>
+            <p class="project-description">Dit is een image uploader voor het screenshot programma ShareX, ik heb dit project gemaakt in Javascript met ExpressJS.</p>
 
         </div>
 
         <div class="about-me image wow">
 
-            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.discordapp.com/banners/563366877028679691/401b24e308a076c5dbb124c4a5d1dfb4.jpg?size=512" alt="Daneric">
+            <img class="right-picture" style="border-radius: 10px;" width="500" src="https://cdn.my-project.nl/cdn/1f3c719723e10893" alt="Daneric">
 
         </div>
     </div>
 
-    
     <hr class="line">
-
 
     <div class="project">
 
